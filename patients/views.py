a95c5996a7a4ff2ebfe0d1bb6112ddc5bbff376b
dhrvjha <kdhruv71@gmail.com>
2022-04-02 19:23:42 +0530

    first commit

---
+++ patients/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views.generic import View
+
+# Create your views here.
+
+
+class PatientsHomeView(View):
+    def get(self, request):
+        return render(request, "patients/main.html")
+
+def testPatientModel(request):
+    return render(request, "patients/test_model.html")
