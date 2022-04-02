20ed888ba350df84aaf521a45e7e3173cf278b02
dhrvjha <kdhruv71@gmail.com>
2022-04-02 22:44:01 +0530

    fixed errors

--- patients/urls.py
+++ patients/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
-from patients.views import PatientsHomeView
+from patients.views import PatientsHomeView, testPatientModel
 
-urlpatterns = [path("", PatientsHomeView.as_view(), name="patientHome"),
-    path("test/", )]
+urlpatterns = [
+    path("", PatientsHomeView.as_view(), name="patientHome"),
+    path("test/", testPatientModel, name="test_model"),
+]
