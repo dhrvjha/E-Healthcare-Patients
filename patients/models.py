b53bfef9d07951288791ad236e4f76063d32535e
dhrvjha <kdhruv71@gmail.com>
2022-04-02 19:23:42 +0530

    first commit

---
+++ patients/models.py
@@ -0,0 +1,28 @@
+import uuid
+
+from django.db import models
+from django.urls import reverse
+
+
+class Patients(models.Model):
+    class PriorityChoices(models.TextChoices):
+        EME = "1", "Emergency"
+        ADM = "2", "Admit"
+        NOR = "3", "Normal"
+
+    id = models.UUIDField(
+        default=uuid.uuid4, blank=True, null=False, primary_key=True, db_index=True
+    )
+
+    firstName = models.CharField(max_length=150, blank=False)
+    lastName = models.CharField(max_length=150, blank=False)
+    age = models.PositiveIntegerField(blank=False)
+    priority = models.CharField(
+        max_length=2, choices=PriorityChoices.choices, default=PriorityChoices.EME
+    )
+
+    def __str__(self):
+        return f"{self.firstName} {self.lastName}"
+
+    def get_absolute_url(self):
+        return reverse("patients-RUD", kwargs={"pk": self.id})
