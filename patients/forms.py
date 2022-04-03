41a994c252cbd357bc380db1abe082874691e582
Rohit ranjan <ranjanrohit562@gmail.com>
2022-04-03 15:37:48 +0530

    login_logout_register

---
+++ patients/forms.py
@@ -0,0 +1,25 @@
+import profile
+from pyexpat import model
+from django import forms
+from django.contrib.auth.models import patients
+from django.contrib.auth.forms import patientsCreationForm
+from.models import Profile
+
+class patientsRegisterForm(patientsCreationForm):
+    email =  forms.EmailField()
+
+    class Meta :
+        model = patients
+        fields = ['firstName','lastName','email','password1','password2']
+
+
+
+class patientsUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta :
+        model = patients
+        fields = ['username','email']
+
+
+
