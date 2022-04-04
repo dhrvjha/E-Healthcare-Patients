cab952bf8d4340849d7e704b53d6f9ba3606c132
Rohit ranjan <ranjanrohit562@gmail.com>
2022-04-04 22:54:56 +0530

    login

--- patients/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from patients.views import PatientsHomeView, testPatientModel
-
-urlpatterns = [
-    path("", PatientsHomeView.as_view(), name="patientHome"),
-    path("test/", testPatientModel, name="test_model"),
-]
