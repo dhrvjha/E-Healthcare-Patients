0b2bcf4e024e34b4f59bc8641f195a65f3c95e65
dhrvjha <kdhruv71@gmail.com>
2022-04-05 04:49:43 +0530

    replaced User with get_user_model

--- patients/signals.py
+++ patients/signals.py
@@ -1,15 +1,19 @@
+"""Save patients after User is saved"""
+
+
+from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
-from django.contrib.auth.models import User
 from django.dispatch import receiver
-from .models import Profile
+
+from .models import Patients
 
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=get_user_model())
 def create_profile(sender, instance, created):
     if created:
-        Profile.objects.create(user=instance)
+        Patients.objects.create(user=instance)
 
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=get_user_model())
 def save_profile(sender, instance):
-    instance.profile.save()
+    instance.patients.save()
