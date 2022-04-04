feb4877128405ea2cd987702049656c76c310cc9
dhrvjha <kdhruv71@gmail.com>
2022-04-05 04:50:23 +0530

    created Patient Detail View

--- patients/views.py
+++ patients/views.py
@@ -1,13 +1,37 @@
+"""Views of patient"""
+
+
+from django import http
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from django.views.generic import View
 
-# Create your views here.
+from .models import Patients
 
 
-class PatientsHomeView(View):
-    def get(self, request):
-        return render(request, "patients/main.html")
+@login_required
+def PatientHomeView(request):
+    """First view of patient after login"""
+    return render(request, "patients/main.html")
+
+
+@login_required
+def PatientDetailsView(request):
+    """Patient detail info view"""
+    try:
+        patient = Patients.objects.get(user=request.user)
+    except Patients.DoesNotExist:
+        return http.HttpResponseNotFound
+    context = {}
+    for key, value in patient.__dict__.items():
+        if key != "user":
+            context[key] = value
+    for key, value in patient.user.__dict__.items():
+        context[key] = value
+    return render(
+        request=request, template_name="patients/detail.html", context=context
+    )
 
 
 def testPatientModel(request):
+    """test view to test ML model on different sets of input"""
     return render(request, "patients/test_model.html")
